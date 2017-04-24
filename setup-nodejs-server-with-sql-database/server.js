13161fff01ba2b3b0d8ba8aa98c82900d2d8f81e
Praveen kumar <praveen.kumar@datametica.com>
2017-04-24 14:28:47 +0530

    angular2 and NodeJs code

---
+++ setup-nodejs-server-with-sql-database/server.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var mysql = require('mysql');
+var bodyParser = require('body-parser')
+var app = express();
+//Start Database Configuration 
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'praveen',
+    database: 'webpackcli'
+});
+//End Database Configuration 
+connection.connect(function(error){
+    if(!!error){
+        console.log('Error')
+    } else{
+        console.log('Connected');
+    }
+})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(function(req,res,next){
+  var _send = res.send;
+  var sent = false;
+  res.send = function(data){
+    if(sent) return;
+    _send.bind(res)(data);
+    sent = true;
+};
+  next();
+});
+app.use(function (req, res, next) {
+    //Enabling CORS 
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept");
+    next();
+});
+var  executeQuery = function(resp, query){             
+     connection.query(query,function(error,rows,fields){
+            if(!!error){
+                    console.log('Error Query')
+                } else{
+                    console.log('Success');
+                   
+                    resp.send(rows)
+                }
+    })
+}
+app.get('/app',function(req,resp){
+     var query = "Select * from personslist";
+     executeQuery (resp, query);
+});
+app.get('/app/:id',function(req,resp){
+  var dataSend={status:true}
+  var dataSendError={status:false}
+  var values = req.params.id;
+  var query = "delete from personslist where PersonID = ?";
+                connection.query(query,values,function(error,rows,fields){
+            if(!!error){
+                    console.log('Error Query')
+                    resp.send(dataSendError)   
+                } else{
+                    console.log('Success');                  
+                    resp.send(dataSend)                    
+                }
+    })
+});
+app.post('/app/person',function(req,resp){
+     resp.send(req.body)
+var values = req.body;
+   var query = "INSERT INTO personslist SET ?";
+     connection.query(query,values,function(error,rows,fields){
+            if(!!error){
+                    console.log('Error Query')
+                } else{
+                    console.log('Success');                  
+                    resp.send(rows)                    
+                }
+    })
+});
+app.listen(4442);
